d77741f0b0d2a70652d36b8c73443fd7f218fd61
EVHEN KOROLOV <fricker12@gmail.com>
2023-07-03 11:56:47 +0300

    add logger

--- DataBasesParser/DataAnalyzer.py
+++ DataBasesParser/DataAnalyzer.py
@@ -1,10 +1,24 @@
 from sqlalchemy import Text, text
 import redis
+import time
+import logging
 class Analyzer:
     def __init__(self, db_connection, db_type):
         self.db_connection = db_connection
         self.db_type = db_type
-
+        
+    @staticmethod
+    def log_execution_time(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            execution_time = time.time() - start_time
+            logger = logging.getLogger(__name__)
+            logger.info(f"Method {func.__name__} executed in {execution_time:.2f} seconds")
+            return result
+        return wrapper
+    
+    @log_execution_time
     def execute_query(self, query):
         if self.db_type == 'mongodb':
             return self.db_connection.collection.find(query)
@@ -16,7 +30,9 @@ class Analyzer:
                 result = connection.execute(query)
                 return result.fetchall()
             
+    @log_execution_time        
     def get_ip_user_agent_statistics(self, n): 
+        
         import_table = self.db_connection.metadata.tables['import']
         forwarded_for = import_table.c.forwarded_for
         user_agent = import_table.c.user_agent
--- run.py
+++ run.py
@@ -10,34 +10,36 @@ stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
+# Остальной код программы...
+
 db_type = 'mysql'
 db_name = 'mydatabase'
 
 db_connection = Connector.DatabaseConnection(db_type, db_name)
 db_connection.connect()
-start_time = time.time()  # Запоминаем время начала выполнения
+
+start_time = time.time()
 db_connection.import_log_data('access_log')
-end_time = time.time()  # Запоминаем время окончания выполнения
-execution_time = end_time - start_time  # Вычисляем время выполнения
-# Выводим информацию о времени выполнения
-logger.info(f"Время выполнения: {execution_time} сек")
+end_time = time.time()
+execution_time_import = end_time - start_time
 
-tart_time = time.time()  # Запоминаем время начала выполнения
 analyzer = DataAnalyzer.Analyzer(db_connection, db_type)
+
+start_time = time.time()
 statistics = analyzer.get_ip_user_agent_statistics(5)
+end_time = time.time()
+execution_time_statistics = end_time - start_time
 
-end_time = time.time()  # Запоминаем время окончания выполнения
-execution_time = end_time - start_time  # Вычисляем время выполнения
-
-for ip_address,referer,balancer_worker_name, user_agent, count in statistics:
+for ip_address, referer, balancer_worker_name, user_agent, count in statistics:
     print(f"IP Address: {ip_address}")
     print(f"User-Agent: {user_agent}")
     print(f"referer:    {referer}")
     print(f"balancer_worker_name:    {balancer_worker_name}")
     print(f"Count: {count}")
     print("-------------------")
-    
+
 # Выводим информацию о времени выполнения
-logger.info(f"Время выполнения: {execution_time} сек")
+logger.info(f"Время выполнения import_log_data: {execution_time_import:.2f} сек")
+logger.info(f"Время выполнения get_ip_user_agent_statistics: {execution_time_statistics:.2f} сек")
 
 db_connection.close()
